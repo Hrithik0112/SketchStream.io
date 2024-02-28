f4ad765c07fa05bd35ba713ca5123fceb2270e5b
Hrithik <slumdog4321@gmail.com>
2024-02-28 21:19:06 +0530

    custom excalidraw whiteboard canvas added

--- app/(routes)/workspace/_components/CanvasWorkspace.tsx
+++ app/(routes)/workspace/_components/CanvasWorkspace.tsx
@@ -1,10 +1,35 @@
 import React from "react";
-import { Excalidraw } from "@excalidraw/excalidraw";
+import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
 
 function CanvasWorkspace() {
   return (
-    <div style={{ height: "500px" }}>
-      <Excalidraw />
+    <div style={{ height: "670px" }}>
+      <Excalidraw
+        theme="light"
+        onChange={(excalidrawElements, appState, files) => console.log(excalidrawElements)}
+        UIOptions={{
+          canvasActions: {
+            saveToActiveFile: false,
+            toggleTheme: false,
+            export: false,
+            loadScene: false,
+          },
+        }}
+      >
+        <MainMenu>
+          <MainMenu.DefaultItems.ClearCanvas />
+          <MainMenu.DefaultItems.SaveAsImage />
+          <MainMenu.DefaultItems.ChangeCanvasBackground />
+        </MainMenu>
+        <WelcomeScreen>
+          <WelcomeScreen.Hints.MenuHint />
+          <WelcomeScreen.Hints.MenuHint />
+          <WelcomeScreen.Hints.ToolbarHint />
+          <WelcomeScreen.Center>
+            <WelcomeScreen.Center.MenuItemHelp />
+          </WelcomeScreen.Center>
+        </WelcomeScreen>
+      </Excalidraw>
     </div>
   );
 }
