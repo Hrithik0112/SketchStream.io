f542853b0a18e5bafd59b98f7b67c78aae53ded2
Hrithik <slumdog4321@gmail.com>
2024-02-28 11:09:42 +0530

    protected route added

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <div>dashboard</div>
+      <Button>
+        <LogoutLink>Logout</LogoutLink>
+      </Button>
+    </>
+  );
 }
 
 export default Dashboard;
