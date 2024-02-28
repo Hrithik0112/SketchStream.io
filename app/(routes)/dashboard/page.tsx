93a38acb56d13c2dcff87cda7bcfe7f8214c0115
Hrithik <slumdog4321@gmail.com>
2024-02-28 11:33:59 +0530

    convex query and mutation

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,9 +1,34 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
-import React from "react";
+import { api } from "@/convex/_generated/api";
+import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useConvex, useMutation } from "convex/react";
+import React, { useEffect } from "react";
 
 function Dashboard() {
+  const convex = useConvex();
+  const { user }: any = useKindeBrowserClient();
+  //const getUser=useQuery(api.user.getUser,{email:user?.email}); this can cause error
+
+  const createUser = useMutation(api.user.createUser);
+  useEffect(() => {
+    if (user) {
+      checkUser();
+    }
+  }, [user]);
+
+  const checkUser = async () => {
+    const result = await convex.query(api.user.getUser, { email: user?.email });
+    if (!result?.length) {
+      createUser({
+        name: user.given_name,
+        email: user.email,
+        image: user.picture,
+      }).then((resp) => {
+        console.log(resp);
+      });
+    }
+  };
   return (
     <>
       <div>dashboard</div>
