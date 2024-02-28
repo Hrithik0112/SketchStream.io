39fe447329a4ba55be6ab8348237c1f5e42582a2
Hrithik <slumdog4321@gmail.com>
2024-02-28 17:19:37 +0530

    added header in dashboard

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { api } from "@/convex/_generated/api";
 import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { useConvex, useMutation } from "convex/react";
 import React, { useEffect } from "react";
+import Header from "./_components/Header";
 
 function Dashboard() {
   const convex = useConvex();
@@ -30,12 +31,9 @@ function Dashboard() {
     }
   };
   return (
-    <>
-      <div>dashboard</div>
-      <Button>
-        <LogoutLink>Logout</LogoutLink>
-      </Button>
-    </>
+    <div className="p-8">
+      <Header />
+    </div>
   );
 }
 
